5815cb52bcdba86b220b2eea84527b8069c35469
ThaminduChankana <it20604530@my.sliit.lk>
2022-10-28 01:58:55 +0530

    updated app js file

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import Context from './API/Context';
+import InshortTabs from './components/InshortTabs';
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{...styles.container,backgroundColor:"#1D3E53"}}>
+      <InshortTabs />
+      
     </View>
   );
 }
@@ -13,8 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
   },
 });
+ 
+export default () => {
+  return (
+    <Context>
+      <App />
+    </Context>
+  );
+};
